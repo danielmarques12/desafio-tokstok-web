96cee66f953c39b5eb074f357fe951c1030a71ce
Daniel Marques <daniel.brz2009@gmail.com>
2021-07-23 20:21:05 -0300

    First commit - Project scaffold

---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch></Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  html, body, #root {
+    min-height: 100%;
+    background: #222;
+    color: #fff;
+  }
+  body, input, button {
+    color: #222;
+    font-size: 14px;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
