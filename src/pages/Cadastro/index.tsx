9b77911e459e7641466a944699f57fd55ee481d7
Daniel Marques <daniel.brz2009@gmail.com>
2021-07-25 20:58:29 -0300

    - Added React Modal dependencie
    - Created a Modal that pops up when a Fornecedor is created

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -2,9 +2,37 @@ import { Field, Form, Formik } from 'formik';
 import { Container } from './styles';
 import { Button } from '@material-ui/core';
 import { api } from '../../services/api';
-import { Fornecedor } from '../Home/interfaces/fornecedor.interface';
+import Modal from 'react-modal';
+import { useState } from 'react';
 
 export function Cadastro() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const modalStyles = {
+    content: {
+      background: '#fff',
+      color: '#000',
+      width: '320px',
+      height: '200px',
+      margin: 'auto',
+      borderRadius: '10px',
+      display: 'flex',
+      justifyContent: 'space-evenly',
+      alignItems: 'center',
+      gap: '2rem',
+      fontSize: '15px',
+    },
+
+    overlay: {
+      background:
+        'linear-gradient(rgba(255, 255, 255, 0.5), rgba(122, 122, 122, 0.5))',
+    },
+  };
+
+  const handleModalClick = () => {
+    document.location.reload();
+  };
+
   return (
     <Container>
       <h1>Cadastrar um fornecedor</h1>
@@ -20,49 +48,61 @@ export function Cadastro() {
             cep: '',
             rua: '',
             numero: '',
-            complemento: ''
-          }
+            complemento: '',
+          },
         }}
         onSubmit={async (values) => {
           await api.post('/fornecedores/create', values);
+          setModalIsOpen(true);
         }}
       >
         <Form>
-          <Field type='text' name='nome' placeholder='Nome'></Field>
+          <Field type="text" name="nome" placeholder="Nome"></Field>
 
           <Field
-            type='text'
-            name='razao_social'
-            placeholder='Razão social'
+            type="text"
+            name="razao_social"
+            placeholder="Razão social"
           ></Field>
 
-          <Field type='text' name='cnpj' placeholder='CNPJ'></Field>
-          <Field type='text' name='segmento' placeholder='Segmento'></Field>
-          <Field type='text' name='telefone' placeholder='Telefone'></Field>
-          <Field type='email' name='email' placeholder='Email'></Field>
-          <Field type='text' name='endereco.cep' placeholder='CEP'></Field>
-          <Field type='text' name='endereco.rua' placeholder='Rua'></Field>
+          <Field type="text" name="cnpj" placeholder="CNPJ"></Field>
+          <Field type="text" name="segmento" placeholder="Segmento"></Field>
+          <Field type="text" name="telefone" placeholder="Telefone"></Field>
+          <Field type="email" name="email" placeholder="Email"></Field>
+          <Field type="text" name="endereco.cep" placeholder="CEP"></Field>
+          <Field type="text" name="endereco.rua" placeholder="Rua"></Field>
 
           <Field
-            type='text'
-            name='endereco.numero'
-            placeholder='Número'
+            type="text"
+            name="endereco.numero"
+            placeholder="Número"
           ></Field>
 
           <Field
-            type='text'
-            name='endereco.complemento'
-            placeholder='Complemento'
+            type="text"
+            name="endereco.complemento"
+            placeholder="Complemento"
           ></Field>
 
           <Button
-            type='submit'
-            variant='contained'
+            type="submit"
+            variant="contained"
             // color='#009845'
-            size='large'
+            size="large"
           >
             Cadastrar
           </Button>
+
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={() => setModalIsOpen(false)}
+            shouldCloseOnOverlayClick={false}
+            style={modalStyles}
+            preventScroll
+          >
+            <h2>Reservation registered successfully</h2>
+            <Button onClick={handleModalClick}>Confirm</Button>
+          </Modal>
         </Form>
       </Formik>
     </Container>
